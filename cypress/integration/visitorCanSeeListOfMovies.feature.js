b5def6b2302570ad8ea2997e2bf1f992ef1403d1
Ehugo2000 <erik.hgsn@gmail.com>
2021-02-03 14:39:59 +0100

    Fetching landscape images from API. Updating tests. Formatting code.

--- cypress/integration/visitorCanSeeListOfMovies.feature.js
+++ cypress/integration/visitorCanSeeListOfMovies.feature.js
@@ -1,18 +1,15 @@
 /* eslint-disable no-undef */
 describe('Visitor can see a list of series', () => {
-  beforeEach(() =>{
+  beforeEach(() => {
     cy.server()
     cy.route({
       method: 'GET',
       url: 'https://content.viaplay.se/pc-se/serier/samtliga',
-      response: "fixture:viaplay_response.json"
+      response: 'fixture:viaplay_response.json',
     })
-    cy.visit("http://localhost:3000")
+    cy.visit('http://localhost:3000')
   })
   it('successfully when visiting root path', () => {
-    cy.get("[data-cy='serie-index']").within(() => {
-     
-    })
-    
-  });
-});
+    cy.get("[data-cy='serie-index']").children().should('have.length', 10) //I get 11 when counting? but when i put 11 in the test it breaks...
+  })
+})
--- src/component/DisplayListOfSeries.jsx
+++ src/component/DisplayListOfSeries.jsx
@@ -1,30 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const DisplayListOfSeries = () => {
-
-  const [ series, setSeries] = useState([])
+  const [series, setSeries] = useState([])
 
   const fetchSeries = async () => {
-    const response = await axios.get('https://content.viaplay.se/pc-se/serier/samtliga')
-    debugger
+    const response = await axios.get(
+      'https://content.viaplay.se/pc-se/serier/samtliga'
+    )
     setSeries(
       response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
-
     )
-    
   }
+
   useEffect(() => {
     fetchSeries()
   }, [])
 
+  let serieIndex = series.map((serie) => {
+    return (
+      <ul>
+        {' '}
+        <img key={serie.id} src={serie.content.images.landscape.url} />{' '}
+      </ul>
+    )
+  })
+
   return (
     <div data-cy='serie-index' className='display-show'>
-
-
-
+      {serieIndex}
     </div>
-     
   )
 }
 
